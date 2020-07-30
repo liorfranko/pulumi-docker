c8dfe43225470bc82cefa3ab33112d438570de4c
liorf <liorf@liveperson.com>
2020-07-30 12:20:17 +0300

    Fix pulumi-docker.sh

--- pulumi-docker.sh
+++ pulumi-docker.sh
@@ -1,2 +1,4 @@
-docker build -t pulumi_docker .
+# shellcheck disable=SC2128
+CODE_LOCATION=$(echo "$BASH_SOURCE" | awk -F 'pulumi-docker' '{print $1 }')
+docker build -t pulumi_docker "$CODE_LOCATION/pulumi-docker/"
 docker run -it pulumi_docker
