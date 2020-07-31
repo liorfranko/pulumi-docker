98dc51312df3fb68830a2f0acb6351e2000863cf
liorf <liorf@liveperson.com>
2020-07-31 14:58:23 +0300

    test

--- pulumi-docker.sh
+++ pulumi-docker.sh
@@ -2,3 +2,7 @@
 CODE_LOCATION=$(echo "$BASH_SOURCE" | awk -F 'pulumi-docker' '{print $1 }')
 docker build -t pulumi_docker "$CODE_LOCATION/pulumi-docker/"
 docker run -it pulumi_docker
+
+
+
+#TEsts
