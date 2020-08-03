c935e05479f989eb0f4a318b201bb64cf89dcae3
liorf <liorf@liveperson.com>
2020-08-03 18:07:23 +0300

    Remove GCP config

--- pulumi-docker.sh
+++ pulumi-docker.sh
@@ -3,7 +3,6 @@ CODE_LOCATION=$(echo "$BASH_SOURCE" | awk -F 'pulumi-docker' '{print $1 }')
 GOOGLE_CREDENTIALS=$(cat /Users/$USER/.shared-network_terraform_user_credentials.json)
 AWS_ACCESS_KEY_ID=$(cat /Users/$USER/.aws/private-aws-access-key)
 AWS_SECRET_ACCESS_KEY=$(cat /Users/$USER/.aws/private-aws-secret)
-# GOOGLE_CREDENTIALS=$(cat /Users/$USER/.gcp-credentials.json)
 PULUMI_ACCESS_TOKEN=$(cat /Users/$USER/.pulumi-creds)
 docker build --build-arg GOOGLE_CREDENTIALS="$GOOGLE_CREDENTIALS" \
 --build-arg PULUMI_ACCESS_TOKEN="$PULUMI_ACCESS_TOKEN" \
